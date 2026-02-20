d45f8d2fe4345a8ce4824d0cbfac84445887c1ec
quinnslab <dsfjghlasdr@gmail.com>
2026-02-20 14:14:44 -0800

    Commit #3: added max FPS rate of 60 for performance

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -13,6 +15,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
     print(f"Starting Asteroids with pygame version:", pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")

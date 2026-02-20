831e2385cec4b377f5df4e987bddf7452518a3ba
quinnslab <dsfjghlasdr@gmail.com>
2026-02-20 15:39:33 -0800

    Commit #4: added player drawing to screen and movement functionailty

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
-        clock.tick(60)
+ 
         dt = clock.tick(60) / 1000
 
-    print(f"Starting Asteroids with pygame version:", pygame.version.ver)
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        updatable.update(dt)
 
+        screen.fill("black")
+        for drawings in drawable:
+            drawings.draw(screen)
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
